96dc00423b3bf9f7e5d9121d1dd6886953595079
T_DaMER <dpancov64@gmail.com>
2025-04-10 09:41:37 +0300

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import tailwindcss from '@tailwindcss/vite'
 import runtimeErrorOverlay from '@replit/vite-plugin-runtime-error-modal'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
@@ -8,7 +8,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), runtimeErrorOverlay(), tsconfigPaths(), tailwindcss()],
-  base: '/mene/',
+  base: '/mene-tracker/',
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, 'src'),
