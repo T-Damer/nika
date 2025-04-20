d9905fed37da9bcc2c518dd18ade2579f7a5b1cf
T_DaMER <dpancov64@gmail.com>
2025-04-20 08:51:37 +0300

    Defaults to ru-lang

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
+import { initReactI18next } from 'react-i18next'
 
 import enTranslation from '../locales/en.json'
 import ruTranslation from '../locales/ru.json'
@@ -19,7 +19,7 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    fallbackLng: 'en',
+    fallbackLng: 'ru',
     interpolation: {
       escapeValue: false,
     },
