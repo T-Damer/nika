a4e66c8ede533fca8045e36e6b33c61abafa0536
T_DaMER <dpancov64@gmail.com>
2025-04-26 21:42:25 +0300

    ru as default

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,33 +1,24 @@
 import i18n from 'i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
 import enTranslation from '../locales/en.json'
 import ruTranslation from '../locales/ru.json'
 
 const resources = {
-  en: {
-    translation: enTranslation,
-  },
   ru: {
     translation: ruTranslation,
   },
+  en: {
+    translation: enTranslation,
+  },
 }
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources,
-    fallbackLng: 'ru',
-    interpolation: {
-      escapeValue: false,
-    },
-    detection: {
-      order: ['localStorage', 'navigator'],
-      lookupLocalStorage: 'i18nextLng',
-      caches: ['localStorage'],
-    },
-  })
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'ru',
+  interpolation: {
+    escapeValue: false,
+  },
+})
 
 export default i18n
